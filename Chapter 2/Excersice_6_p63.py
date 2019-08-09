905eee680d41baf89e256b1e8e1dcc663897a656
niniyao <jingrongyao121@gmail.com>
2019-08-08 22:52:06 -0400

    finishing chapter 2

---
+++ Chapter 2/Excersice_6_p63.py
@@ -0,0 +1,18 @@
+a = 2 + 3
+print("a =", a)
+b = 3 - 2
+print("b =", b)
+c = 2 * 3
+print("c =", c)
+d = 3 / 2
+print("d =", d) 
+
+e = 3 ** 2
+print("e =", e)
+f = 3 ** 3
+print("f =", f)
+g = 10 ** 6
+print("g =", g)
+
+
+
---
+++ Chapter 2/Excersice_8_66.py
@@ -0,0 +1,10 @@
+print(5+3)
+print(10-2)
+print(2*4)
+print(int(8/1))
+print(float(8/1))
+#除数返回值默认为浮点数。
+
+a = 7
+print("My favorate number is " + str(a) + ".")
+
