b293d4a894ef23d8c143333b411dfb20af19ceb4
niniyao <jingrongyao121@gmail.com>
2019-12-15 23:56:30 -0500

    review loop & add setting file for AI game

--- Pygame/alien_invation.py
+++ Pygame/alien_invation.py
@@ -1,10 +1,14 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
-    screen = pygame. display.set_mode((800, 600))
+    ai_settings = Settings()
+    screen = pygame. display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Start the main loop for the game.
@@ -15,8 +19,13 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
 run_game()
+
+
 
