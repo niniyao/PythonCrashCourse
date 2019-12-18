b78c1d82a6fd0392efd4cacfbde7610630a22c84
niniyao <jingrongyao121@gmail.com>
2019-12-17 21:53:36 -0500

    Finish the chapter 5

---
+++ Chapter 5/example 3.py
@@ -0,0 +1,14 @@
+ages = [10, 20, 30, 40, 50]
+for age in ages:
+    if age == 3:
+        print("It is not on the list.")
+    elif age == 10:
+        print("Children")
+    elif age == 20:
+        print("Teenager")
+    elif age == 30:
+        print("Middle age")
+    else:
+        print("It is not on the list.")
+
+
